39f898c43e7d0e0fcadbf8923b3935580f9cae2c
c011918543 <88069298+c011918543@users.noreply.github.com>
2022-07-11 10:28:16 +0900

    Update and rename Abort.sh to Limit.sh

--- Limit.sh
+++ Limit.sh
@@ -1,16 +1,16 @@
-function abort () {
+function limit () {
 watch=(-e CREATE -e CLOSE_WRITE -m)
 inotifywait ${watch[@]} /home/futa/newkanshi | while read notice
 do
 if [ "`echo $notice | awk "{print \$2;}" | grep CREATE`" ]
 then
-    echo "バックアップ中断"
-    killall -SIGSTOP rsync
+    echo "バックアップ制限"
+    #limit_command
 else
-    echo "バックアップ再開"
-    killall -SIGCONT rsync
+    echo "バックアップ制限解除"
+    #rsync
 fi
 done
 }
 
-abort &
+limit &
