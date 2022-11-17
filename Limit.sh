28247ee7bb84bb565e88086982ddc2c06ba3b4dd
c011918543 <88069298+c011918543@users.noreply.github.com>
2022-11-17 16:25:07 +0900

    Update Limit.sh

--- Limit.sh
+++ Limit.sh
@@ -1,14 +1,38 @@
+dirname=ディレクトリ名
+echo "指定ディレクトリ:"$dirname
+filesize=$(ls -lh $dirname | awk '{sum+=$5} END {print sum}')
+echo "合計ファイルサイズ"$filesize"GB"
+tlimit=バックアップ期限
+echo "後"$(($tlimit / 60))"分"
+limit_value=$(($filesize * 1024 / $tlimit))"MB/s"
+
 function limit () {
 watch=(-e CREATE -e CLOSE_WRITE -m)
-inotifywait ${watch[@]} /home/futa/newkanshi | while read notice
+inotifywait ${watch[@]} 監視ディレクトリ | while read notice
 do
 if [ "`echo $notice | awk "{print \$2;}" | grep CREATE`" ]
 then
-    echo "バックアップ制限"
-    #limit_command
+    echo "帯域幅上限変更"
+    killall -SIGSTOP rsync
+    expect -c "
+      spawn rsync -avhP --bwlimit=$limit_value ファイル名 user@IPアドレス:~/
+      echo $limit_value
+      expect \"password:\"
+      send -- \"パスワード\r\"
+      expect eof
+      exit
+    "
 else
-    echo "バックアップ制限解除"
-    #rsync
+    echo "バックアップ再開"
+    killall -SIGSTOP rsync
+    expect -c "
+      spawn rsync -avhP ファイル名 user@IPアドレス:~/
+      echo $limit_value
+      expect \"password:\"
+      send -- \"パスワード\r\"
+      expect eof
+      exit
+    "
 fi
 done
 }
